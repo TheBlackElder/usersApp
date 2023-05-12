61d7b470c0484d6b4365e78a90422e36ace71e29
TheBlackElder <freedxcollective@gmail.com>
2023-05-12 16:20:19 -0400

    add new user form and route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const users = {
   userID: {
+    id: "userID",
     firstName: "Landon",
     lastName: "Pryce",
     dateOfBirth: `May 16 1992`,
@@ -14,6 +17,7 @@ const users = {
     bio: `I hail from the streets of Scarborough as a self-taught multimedia artist versed in writing poetry, rapping, playing piano and creating visual and digital art.`,
   },
   user2ID: {
+    id: "user2ID",
     firstName: "Amelia",
     lastName: "Gracious",
     dateOfBirth: `May 22 1999`,
@@ -21,6 +25,7 @@ const users = {
     bio: `I hail from the streets of Scarborough as a self-taught multimedia artist versed in writing poetry, rapping, playing piano and creating visual and digital art.`,
   },
   user3ID: {
+    id: "user3ID",
     firstName: "Leo",
     lastName: "Clarke",
     dateOfBirth: `April 16 1992`,
@@ -29,20 +34,32 @@ const users = {
   },
 };
 
-// const profileForUser = (userID, urlDatabase) => {
-//   let result = {};
-//   for (const id in users) {
-//     if (urlDatabase[id].userID  === userID) {
-//       result[id] = urlDatabase[id];
-//     }
-//   }
-//   return result;
-// };
+const profileForUser = (users) => {
+  let result = {};
+  for (const id in users) {
+    if (users[id]) {
+      result[id] = users;
+    }
+  }
+  return result;
+};
+
+const generateRandomString = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 
 app.get("/", (req, res) => {
   res.status(200);
   const templateVars = {
-    user: users,
+    users,
   };
   res.render("users_index", templateVars);
 });
@@ -50,33 +67,74 @@ app.get("/", (req, res) => {
 
 app.get("/users/new", (req, res) => {
   const templateVars = {
-    user: users,
+ users,
   };
   res.render("users_new", templateVars);
 });
 
-app.get("/users/:id", (req, res) => {
+// app.post("/users/new", (req, res) => {
+//   res.render("/");
+// });
 
-  if (users) {
+// app.post("/", (req, res) => {
+//   const firstName = req.body.firstName;
+//   const lastName = req.body.lastName;
+//   const userBirthday = req.body.dateOfBirth;
+//   const userPicture = req.body.profilePicture;
+//   const userBio = req.body.bio;
+//   const templateVars = {
+//  firstName,lastName, userBirthday, userPicture, userBio
+//   };
+//   res.redirect("/", templateVars);
+// });
+
+app.post("/", (req, res) => {
+  const newUserID = generateRandomString();
+  const {firstName, lastName, dateOfBirth, profilePicture, bio } = req.body
+  users[newUserID] = 
+  {newUserID, firstName, lastName, dateOfBirth, profilePicture, bio }
+
+  const newUser = {
+    firstName, lastName, dateOfBirth, profilePicture, bio
+  }
+  // users.id.push(newUser);
+
+  res.render('users_index', { users });
+  res.redirect("/");
+});
+
+// app.get("/users/:id", (req, res) => {
+
+//   if (profileForUser(users)) {
+//     const templateVars = {
+//       user: users,
+//     };
+//     return res.render("users_show", templateVars);
+//   }
+// });
+
+
+
+app.get("/users/:id", (req, res) => {
+  const userID = req.params.userId;
+  if (profileForUser(userID, users)) {
     const templateVars = {
+      id: userID,
       user: users,
     };
     return res.render("users_show", templateVars);
+
   }
 });
 
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.users[id];
+ if (users.hasOwnProperty(id)) {
+    delete users[id];
+ }
+    res.status(200).send('User deleted successfully');
 
-// app.post("/urls/:shortURL/delete", (req, res) => {
-//   const userID = req.session.userId;
-//   const authURLs = urlsForUser(userID, urlDatabase);
-//   if (authURLs) {
-//     delete urlDatabase[req.params.shortURL];
-//     return res.redirect("/urls");
-//   } else {
-//     res.status(401).send("<html><h3>Access denied<h3><html>");
-//     res.redirect("/login");
-//   }
-// });
+});
 
 
 // app.post("/users/:id/show", (req, res) => {
