bb33403a59edbfcc792b9b3f38aa7964a20cfacc
TheBlackElder <freedxcollective@gmail.com>
2023-05-12 10:43:52 -0400

    establish index page

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
   
 const app = express();
 const PORT = 3000;
-  
-  
+
+app.use(express.urlencoded({ extended: true }));
+// app.set('views', path.join(_dirname, 'views'))
+app.set("view engine", "ejs");
+
+      
 app.get('/', (req, res)=>{
     res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.render("users_index");
 });
 
 app.listen(PORT, (error) =>{
